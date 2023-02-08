c8a49fb1c937372a3e6f21c242b995c9431b6b9d
SOME1HING <yashprakash2005@gmail.com>
2023-02-08 15:54:46 +0530

    Function name of both `sauce` in `/example.py` to `img_quote` and `text_quote` respectively.

--- example.py
+++ example.py
@@ -7,12 +7,12 @@ pbot = Client("PyroBot", api_id="API_ID", api_hash="API_HASH", bot_token="TOKEN"
 
 
 @pbot.on_message(filters.command("quote"))
-async def sauce(_, message: Message):
+async def img_quote(_, message: Message):
     quote_url = await Q.quote()
     return await message.reply_photo(photo=quote_url)
 
 
 @pbot.on_message(filters.command("tquote"))
-async def sauce(_, message: Message):
+async def text_quote(_, message: Message):
     text_quote = await Q.text_quote()
     return await message.reply_text(text_quote)
